df4a3f74cc0cba38747163c665598aaf3cd1dcb6
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-08 20:24:31 +0600

    Set credentials as env variables

--- 51-Internet-speed-Twitter-complain-bot/main.py
+++ 51-Internet-speed-Twitter-complain-bot/main.py
@@ -16,8 +16,8 @@ options.add_experimental_option("detach", True)
 
 PROMISED_UP = 150
 PROMISED_DOWN = 10
-TWITTER_EMAIL = "AriaSmi91565684"
-TWITTER_PASS = "EAm*25?^#&p=J?j"
+TWITTER_EMAIL = os.environ.get("twi_email")
+TWITTER_PASS = "twi_pass"
 
 class InternetSpeedTwitterBot:
     def __init__(self):
@@ -30,10 +30,15 @@ class InternetSpeedTwitterBot:
         time.sleep(3)
         go_button = self.driver.find_element(By.CSS_SELECTOR, ".start-button a")
         go_button.click()
+        time.sleep(60)
+        self.up = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
+        self.down = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span').text
 
     def tweet_at_provider(self):
         pass
 
-bot = InternetSpeedTwitterBot()
-bot.get_internet_speed()
-bot.tweet_at_provider()
+# bot = InternetSpeedTwitterBot()
+# bot.get_internet_speed()
+# bot.tweet_at_provider()
+
+# test =
