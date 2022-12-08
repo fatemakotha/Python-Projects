8b6e9b353407361f8a4dd66a5977d36d0ed99ce4
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-08 20:05:31 +0600

    Create bot class

--- 51-Internet-speed-Twitter-complain-bot/main.py
+++ 51-Internet-speed-Twitter-complain-bot/main.py
@@ -1,4 +1,16 @@
+
+
 PROMISED_UP = 150
 PROMISED_DOWN = 10
 TWITTER_EMAIL = "AriaSmi91565684"
 TWITTER_PASS = "EAm*25?^#&p=J?j"
+
+class InternetSpeedTwitterBot:
+    def __init__(self, up, down):
+        pass
+
+    def get_internet_speed(self):
+        pass
+
+    def tweet_at_provider(self):
+        pass
