c0d7c6d31604c253aa7b6f78d7222948759ea3ab
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-08 21:41:14 +0600

    Complete

--- 51-Internet-speed-Twitter-complain-bot/main.py
+++ 51-Internet-speed-Twitter-complain-bot/main.py
@@ -36,21 +36,27 @@ class InternetSpeedTwitterBot:
     def tweet_at_provider(self):
         self.driver.get("https://twitter.com/login")
         time.sleep(2)
+
         email = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[1]/label/div/div[2]/div/input')
         email.send_keys(TWITTER_EMAIL)
         time.sleep(5)
+
         password = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[2]/label/div/div[2]/div/input')
         password.send_keys(TWITTER_PASS)
         time.sleep(5)
 
+        tweet_compose = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div/div/div/div')
+        tweet = f"Hey Internet Provider, why is my internet speed {self.down}down/{self.up}up when I pay for {PROMISED_DOWN}down/{PROMISED_UP}up?"
+        tweet_compose.send_keys(tweet)
+        time.sleep(3)
+
+        tweet_button = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[4]/div/div/div[2]/div[3]')
+        tweet_button.click()
+        time.sleep(2)
+        self.driver.quit()
 
 
-# bot = InternetSpeedTwitterBot()
-# bot.get_internet_speed()
-# # bot.tweet_at_provider()
+bot = InternetSpeedTwitterBot()
+bot.get_internet_speed()
+bot.tweet_at_provider()
 
-driver = webdriver.Chrome(chrome_options=options)
-driver.get("https://twitter.com/login")
-time.sleep(3)
-email = driver.find_element(By.CLASS_NAME, "r-4qtqp9 r-yyyyoo r-z80fyv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-19wmn03")
-email.send_keys("dfgdf")
