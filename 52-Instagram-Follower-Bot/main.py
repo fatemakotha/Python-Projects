f2551faf63f3c1ea403cd0099b48fe49efdcffaf
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-09 17:40:25 +0600

    Find follows by fetching another link

--- 52-Instagram-Follower-Bot/main.py
+++ 52-Instagram-Follower-Bot/main.py
@@ -12,6 +12,8 @@ import time
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 
+SIMILAR_ACCOUNT = "chefsteps"
+
 #Instagram credentials:
 INSTA_EMAIL = os.environ.get("insta_email")
 INSTA_PASS = os.environ.get("insta_pass")
@@ -21,37 +23,40 @@ class InstaFollower:
     def __init__(self):
         self.driver = webdriver.Chrome(chrome_options=options)
     def login(self):
-        # self.driver.get("https://www.instagram.com/accounts/login/")
-        # username = self.driver.find_element()
+        self.driver.get("https://www.instagram.com/accounts/login/")
+        time.sleep(3)
+        time.sleep(3)
+        username = self.driver.find_element(By.NAME, "username")
+        username.send_keys(INSTA_EMAIL)
+        time.sleep(3)
+        password = self.driver.find_element(By.NAME, "password")
+        password.send_keys(INSTA_PASS)
+        time.sleep(3)
+        login_button = self.driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]/button/div')
+        login_button.click()
+        time.sleep(8)
+        not_now_button = self.driver.find_element(By.CSS_SELECTOR, "._ab8w ._ac8f button")
+        not_now_button.click()# works
+        time.sleep(4)
+        # make sure to copy the FULL XPATH or it wont work **
+        notif_off = self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]')
+        notif_off.click()
+
         pass
     def find_followers(self):
-        pass
+        time.sleep(5)
+        self.driver.get(f"https://www.instagram.com/{SIMILAR_ACCOUNT}")
+        time.sleep(2)
     def follow(self):
         pass
 
-# bot = InstaFollower()
-# bot.login()
-# bot.find_followers()
+bot = InstaFollower()
+bot.login()
+bot.find_followers()
 # bot.follow()
 
-driver = webdriver.Chrome(options=options)
-driver.get("https://www.instagram.com/accounts/login/")
-time.sleep(3)
-username = driver.find_element(By.NAME, "username")
-username.send_keys(INSTA_EMAIL)
-time.sleep(3)
-password = driver.find_element(By.NAME, "password")
-password.send_keys(INSTA_PASS)
-time.sleep(3)
-login_button = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]/button/div')
-login_button.click()
-time.sleep(8)
-not_now_button = driver.find_element(By.CSS_SELECTOR, "._ab8w ._ac8f button")
-not_now_button.click() #works
-time.sleep(4)
-#make sure to copy the FULL XPATH or it wont work **
-notif_off = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]')
-notif_off.click()
 
-search = driver.find_element(By.CLASS_NAME, "_ab6-")
-search.send_keys("chefsteps")
+
+
+
+
