73261dbaa1d649c7ba27d1e8f3e0df46fa4d5fed
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-09 15:43:55 +0600

    Import selenium

---
+++ 52-Instagram-Follower-Bot/main.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+#To press enter and everything:
+from selenium.webdriver.common.keys import Keys
+import os
+import time
+
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
